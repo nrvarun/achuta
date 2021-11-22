6027a6c1c3dfb9d698ed35b9ed1512cba4d4d522
varun <inr.varun89@gmail.com>
2021-11-22 21:27:35 +0530

    all changes done

--- pages/check.tsx
+++ pages/check.tsx
@@ -26,7 +26,7 @@ const Check: NextPage = () => {
         <div className="py-12 md:py-16 xl:py-24">
           <div className="flex">
             <div className="w-1/2">
-              <Link href="/zillskill">
+              <Link href="/angelone-web-portfolio">
                 <a className="flex">
                   <div className="flex">
                     <div className="w-4 h-4 md:w-6 md:h-6 lg:w-9 lg:h-9 flex content-center m-auto">
@@ -41,11 +41,36 @@ const Check: NextPage = () => {
                   </div>
                   <div className="ml-4 md:ml-6">
                     <h4 className="text-white text-base sf-bold md:text-xl lg:text-2xl xl:text-3.5xl mb-2 md:mb-3">
-                      ZILLSKILL
+                      AngelOne
                     </h4>
                     <p className="text-white text-xs  md:text-base xl:text-md sf-bold">
+                      Web portfolio manager
+                    </p>
+                  </div>
+                </a>
+              </Link>
+            </div>
+            <div className="w-1/2 text-right">
+              <Link href="/zillskill">
+                <a className="flex">
+                  <div className="mr-4 md:mr-6 ml-auto">
+                    <h4 className="text-white text-base md:text-xl lg:text-2xl xl:text-3.5xl mb-1 md:mb-3 sf-bold">
+                      zillskill
+                    </h4>
+                    <p className="text-white text-xs md:text-base xl:text-md sf-bold">
                       A learning community for kids
                     </p>
+                  </div>
+                  <div className="flex">
+                    <div className="w-4 h-4 md:w-6 md:h-6 lg:w-9 lg:h-9 flex content-center m-auto">
+                      <Image
+                        src="/icons/arrow-white-right.svg"
+                        width={36}
+                        height={36}
+                        className="rotate-180"
+                        alt="previous case study"
+                      />
+                    </div>
                   </div>
                 </a>
               </Link>
--- pages/zillskill.tsx
+++ pages/zillskill.tsx
@@ -26,7 +26,7 @@ const ZillSkill: NextPage = () => {
         <div className="py-12 md:py-16 xl:py-24">
           <div className="flex">
             <div className="w-1/2">
-              <Link href="/angelone-web-portfolio">
+              <Link href="/check">
                 <a className="flex">
                   <div className="flex">
                     <div className="w-4 h-4 md:w-6 md:h-6 lg:w-9 lg:h-9 flex content-center m-auto">
@@ -41,36 +41,11 @@ const ZillSkill: NextPage = () => {
                   </div>
                   <div className="ml-4 md:ml-6">
                     <h4 className="text-white text-base sf-bold md:text-xl lg:text-2xl xl:text-3.5xl mb-2 md:mb-3">
-                      AngelOne
-                    </h4>
-                    <p className="text-white text-xs  md:text-base xl:text-md sf-bold">
-                      Web portfolio manager
-                    </p>
-                  </div>
-                </a>
-              </Link>
-            </div>
-            <div className="w-1/2 text-right">
-              <Link href="/check">
-                <a className="flex">
-                  <div className="mr-4 md:mr-6 ml-auto">
-                    <h4 className="text-white text-base md:text-xl lg:text-2xl xl:text-3.5xl mb-1 md:mb-3 sf-bold">
                       Check
                     </h4>
                     <p className="text-white text-xs  md:text-base xl:text-md sf-bold">
                       Citizen-driven violation reporting app
                     </p>
-                  </div>
-                  <div className="flex">
-                    <div className="w-4 h-4 md:w-6 md:h-6 lg:w-9 lg:h-9 flex content-center m-auto">
-                      <Image
-                        src="/icons/arrow-white-right.svg"
-                        width={36}
-                        height={36}
-                        className="rotate-180"
-                        alt="previous case study"
-                      />
-                    </div>
                   </div>
                 </a>
               </Link>
